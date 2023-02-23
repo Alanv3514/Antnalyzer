c51c2abe87e1dc10b1d37d7f516acf435998ae50
quiro1297 <quiro1297@gmail.com>
2023-02-23 15:40:06 -0300

    Agregado de vector donde se guarda coordenadas y area

--- pruebavideo.py
+++ pruebavideo.py
@@ -9,8 +9,8 @@ limitemayor= [85, 255, 255]
 menor=np.array(limitemenor, dtype="uint8")
 mayor=np.array(limitemayor, dtype="uint8")
 cap=cv2.VideoCapture(r"C:\Users\Agustin\Videos\test_1.mp4")
-frametime= 10
-detector= cv2.createBackgroundSubtractorMOG2()
+frametime= 16
+detector= cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=15)
 
 while True:
     ret, img2= cap.read()
@@ -18,19 +18,20 @@ while True:
 
     img2hsv=cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
     mask_1=detector.apply(roi)
-    _, mask_1= cv2.threshold(mask_1, 254, 255, cv2.THRESH_BINARY)
+    _, mask_1= cv2.threshold(mask_1, 128, 255, cv2.THRESH_BINARY)
     mask=cv2.inRange(img2hsv, menor, mayor)
     contorno, jerarquia = cv2.findContours(mask_1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    detecciones = []
     for c in contorno:
         area=cv2.contourArea(c)
-        if area > 10:
+        if area > 20:
             x,y,w,h=cv2.boundingRect(c)
             nuevContorno=cv2.convexHull(c)
             cv2.rectangle(roi,(x,y), (x+w,y+h), (0, 255, 0), 1)
-            
+            detecciones.append([x,y,area  ])
             #cv2.putText(img2, 'Hoja',(x-3,y-3),font, 0.75, (0, 255, 0), 1, cv2.LINE_AA)
-            #cv2.drawContours(img2, [nuevContorno], 0, (0,255,0), 2)
+            #cv2.drawContours(roi, [nuevContorno], 0, (0,255,0), 2)
+    print(detecciones)
     cv2.imshow('Mask', mask_1)
     cv2.imshow('Region de interes',roi)
     if cv2.waitKey(frametime) == ord('q'):
